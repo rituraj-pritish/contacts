3599bfc0f2bc8e1ef9506f3c2d8675f5f82ee641
rituraj-pritish <riturajpritish@gmail.com>
2019-09-16 22:29:45 +0530

    heroku setup

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
-const app = express()
+const app = express();
 
 //Init Middleware
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }));
 
 //Define Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contact', require('./routes/contact'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contact', require('./routes/contact'));
+
+//Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('server started'))
+app.listen(PORT, () => console.log('server started'));
