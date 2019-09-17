8abe22fcceb1b4b648ef236f7dab980704f69e3c
rituraj-pritish <riturajpritish@gmail.com>
2019-09-17 10:33:28 +0530

    optimized

--- config/dev.js
+++ config/dev.js
@@ -1,16 +1,6 @@
 module.exports = {
-  GOOGLE: {
-    clientID:
-      '192553611754-qnqgcmn7shd8vfi1hvq1engehesgd0ho.apps.googleusercontent.com',
-    clientSecret: 'QowiMX1vTDLANFshYpGUvyrV'
-  },
-  GITHUB: {
-    clientID: '08e5bf81b60a1652c2b2',
-    clientSecret: '46a76282f671022301ec347f2ca4411dfcc7fcee'
-  },
   mongoUri:
     'mongodb+srv://rp:vinny007@auth-dev-xtbqx.mongodb.net/test?retryWrites=true&w=majority',
-  jwtSecret: 'asdfghjkl',
-  cookieKey: 'asdfqwerpoiulkjh'
+  jwtSecret: 'asdfghjkl'
 };
 
